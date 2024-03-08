072b70eebf6958ed9405f2159e9d460071f05a14
Anderson Parra <ajr.parra@yavirac.edu.ec>
2024-03-07 19:02:19 -0500

    perdon bebe

---
+++ script.js
@@ -0,0 +1,7 @@
+// Obtener el botón
+const myButton = document.getElementById("myButton");
+
+myButton.addEventListener("click", function () {
+  const messageElement = document.getElementById("message");
+  messageElement.innerText = "Te amo, enojona 💘";
+});
